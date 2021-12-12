9fe24bde0568360b70e3e019c55653253fe2574f
Isabelly Dias <isabellydias29@gmail.com>
2021-12-12 12:59:05 -0300

    deploy com buildpack java

--- gerenciador-orcamento/src/main/java/br/com/giorni/gerenciadororcamento/GerenciadorOrcamentoApplication.java
+++ gerenciador-orcamento/src/main/java/br/com/giorni/gerenciadororcamento/GerenciadorOrcamentoApplication.java
@@ -11,6 +11,7 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 @SpringBootApplication
 public class GerenciadorOrcamentoApplication {
 
+	//teste deploy
 	public static void main(String[] args) {
 		SpringApplication.run(GerenciadorOrcamentoApplication.class, args);
 	}
